3d2df43709363c24ef6916f5f44e6a65bef7eda5
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2022-12-12 00:09:37 +0530

    Added Update Employee functionality

--- FullStack API/FullStack.API/FullStack.API/Controllers/EmployeesController.cs
+++ FullStack API/FullStack.API/FullStack.API/Controllers/EmployeesController.cs
@@ -45,5 +45,25 @@ namespace FullStack.API.Controllers
 
             return Ok(employee);
         }
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> UpdateEmployee([FromRoute] Guid id, Employee updateEmployeeRequest)
+        {
+            var employee = await _fullStackDbContext.Employees.FindAsync(id);
+
+            if (employee is null)
+                return NotFound();
+
+            employee.Name = updateEmployeeRequest.Name;
+            employee.Email = updateEmployeeRequest.Email;
+            employee.Phone = updateEmployeeRequest.Phone;
+            employee.Salary = updateEmployeeRequest.Salary;
+            employee.Department = updateEmployeeRequest.Department;
+
+            await _fullStackDbContext.SaveChangesAsync();
+
+            return Ok(employee);
+        }
     }
 }
