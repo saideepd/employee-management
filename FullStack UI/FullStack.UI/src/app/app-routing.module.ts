43b5efcea9403ef260f5d8e7c68c85936c4d6e16
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2022-12-11 21:16:27 +0530

    Added Add Employee component

--- FullStack UI/FullStack.UI/src/app/app-routing.module.ts
+++ FullStack UI/FullStack.UI/src/app/app-routing.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddEmployeeComponent } from './components/employees/add-employee/add-employee.component';
 import { EmployeesListComponent } from './components/employees/employees-list/employees-list.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: EmployeesListComponent
+    component: EmployeesListComponent,
   },
   {
     path: 'employees',
-    component: EmployeesListComponent
-  }
+    component: EmployeesListComponent,
+  },
+  {
+    path: 'employees/add',
+    component: AddEmployeeComponent,
+  },
 ];
 
 @NgModule({
