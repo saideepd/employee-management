d8ace7f7bd12459c3ae459d4cf48f86ddb5b283f
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2022-12-11 21:40:49 +0530

    Added Data binding to form elements

--- FullStack UI/FullStack.UI/src/app/components/employees/add-employee/add-employee.component.ts
+++ FullStack UI/FullStack.UI/src/app/components/employees/add-employee/add-employee.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Employee } from 'src/app/models/employee.model';
 
 @Component({
   selector: 'app-add-employee',
   templateUrl: './add-employee.component.html',
-  styleUrls: ['./add-employee.component.css']
+  styleUrls: ['./add-employee.component.css'],
 })
 export class AddEmployeeComponent {
+  addEmployeeRequest: Employee = {
+    id: '',
+    name: '',
+    email: '',
+    phone: 0,
+    salary: 0,
+    department: '',
+  };
 
+  addEmployee() {
+    console.log(this.addEmployeeRequest);
+  }
 }
