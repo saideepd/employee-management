5f47a4b6bb9bd509dfc91b68299966840ad7ed7f
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2022-12-11 23:37:51 +0530

    Implemented Get Employee By Id & displaying the record field values in Edit Employee component

--- FullStack UI/FullStack.UI/src/app/components/employees/edit-employee/edit-employee.component.ts
+++ FullStack UI/FullStack.UI/src/app/components/employees/edit-employee/edit-employee.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Employee } from 'src/app/models/employee.model';
+import { EmployeesService } from 'src/app/services/employees.service';
 
 @Component({
   selector: 'app-edit-employee',
   templateUrl: './edit-employee.component.html',
-  styleUrls: ['./edit-employee.component.css']
+  styleUrls: ['./edit-employee.component.css'],
 })
-export class EditEmployeeComponent {
+export class EditEmployeeComponent implements OnInit {
+  employeeDetails: Employee = {
+    id: '',
+    name: '',
+    email: '',
+    phone: 0,
+    salary: 0,
+    department: '',
+  };
 
+  constructor(
+    private route: ActivatedRoute,
+    private employeeService: EmployeesService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe({
+      next: (params) => {
+        const id = params.get('id');
+        if (id) {
+          this.employeeService.getEmployee(id)
+          .subscribe({
+            next: (response) => {
+              this.employeeDetails = response;
+            }
+          });
+        }
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
 }
