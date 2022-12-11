c244778a36c3f8e053c70da0e664f3f3dc6647dc
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2022-12-11 20:02:02 +0530

    Added Employee Controller & implemented Get All Employees()

---
+++ FullStack API/FullStack.API/FullStack.API/Controllers/EmployeesController.cs
@@ -0,0 +1,25 @@
+﻿using FullStack.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStack.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeesController : Controller
+    {
+        private readonly FullStackDbContext _fullStackDbContext;
+
+        public EmployeesController(FullStackDbContext fullStackDbContext)
+        {
+            _fullStackDbContext = fullStackDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var employees = await _fullStackDbContext.Employees.ToListAsync();
+            return Ok(employees);
+        }
+    }
+}
